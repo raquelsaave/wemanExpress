aad21cdb7d22739d1ca9c8a40fc2ff08be25607d
Raquel Saavedra <raquel_2496@hotmail.com>
2019-05-16 11:06:48 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict";
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => console.log('El servidor esta corriendo'));
+
+
+app.all('/hello', (request, response) => {
+    response.send('hello world!');
+});
+app.all('/bye', (request, response) => {
+    response.send('Bye bye!');
+});
+
+app.use('/assets',express.static('assets'));
